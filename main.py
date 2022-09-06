85c6de50cff89c234309935044aaade020b8eca8
sunny14 <faina.private@gmail.com>
2022-09-06 21:19:25 +0300

    урок 4 - атака XSS

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 with open('app/form.html', 'r') as f:
     form = f.read()
@@ -21,3 +21,23 @@ async def root():
 @app.get("/tehi")
 async def say_hello():
     return HTMLResponse(texthi)
+
+
+@app.get("/greeting", response_class=HTMLResponse)
+async def api_data(request: Request):
+    name = request.query_params.get("name")
+    return """
+       <html>
+           <head>
+               <title>Some HTML in here</title>
+           </head>
+           <body onload="myFunction()">
+
+               <script>
+                    function myFunction() {
+                        alert('Его величество """+name + """')
+                    }
+                </script>
+               </body>
+       </html>
+       """
